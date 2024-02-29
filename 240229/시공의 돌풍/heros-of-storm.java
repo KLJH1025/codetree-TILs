1159cb8c7d35edb463ba99d649ed6a5a8184167f
JaeHyuk Lee <99160103+KLJH1025@users.noreply.github.com>
2024-02-29 20:41:04 +0900

    Title: 시공의 돌풍, Time: 652ms, Memory: 126MB, Status: Passed - CodeTree

--- 240229/시공의 돌풍/heros-of-storm.java
+++ 240229/시공의 돌풍/heros-of-storm.java
@@ -1,177 +1,165 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final int BLAST = -1;
+    static final int BLAST_SIZE = 2;
+    static int n,m,t;
+    static int[][] map;
+    static int[] blast;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
 
-    static class Point {
-        int y, x;
-        Point(int y, int x) {
-            this.y = y;
+    static class Location{
+        int x;
+        int y;
+
+        public Location(int x, int y){
             this.x = x;
+            this.y = y;
         }
     }
 
-    static int N, M, T;
-    static int[][] map, copyMap;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, - 1};
-    static ArrayList<Point> blast;
-
-    // start: 19:30
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        blast = new ArrayList<>();
-
-        for(int i=0; i<N; i++) {
+        map = new int[n][m];
+        blast = new int[BLAST_SIZE];
+        int blastIdx = 0;
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++) {
+            for(int j=0;j<m;j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
 
-                if(map[i][j] == -1) {
-                    blast.add(new Point(i, j));
+                if(map[i][j] == BLAST){
+                    blast[blastIdx++] = i;
                 }
             }
         }
 
-        for(int t=0; t<T; t++) {
-            // 복사 맵 초기화
-            copyMap = new int[N][M];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    copyMap[i][j] = 0;
-                }
+        while(t-->0){
+            map = changeMap();
+            rotateUp();
+            rotateDown();
+            for(int x:blast){
+                map[x][0] = BLAST;
             }
-
-            // 먼지 확산
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    dustSpread(i, j);
-                }
-            }
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    map[i][j] += copyMap[i][j];
-                }
-            }
-
-            // 먼지 이동
-            upWind();
-            downWind();
         }
 
-//        for(int i=0; i<N; i++) {
-//            for(int j=0; j<M; j++) {
-//                System.out.print(map[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(map[i][j] == BLAST){
+                    continue;
+                }
+                sum += map[i][j];
+            }
+        }
 
-        // 정답 출력
-        System.out.println(countDustAnswer());
+        bw.write(Integer.toString(sum));
+        bw.flush();
+
+        br.close();
+        bw.close();
     }
 
-    // 위쪽 바람
-    public static void upWind() {
-        Point upBlast = blast.get(0);
-        int upBlastY = upBlast.y;
+    public static int[][] changeMap(){
+        int[][] changedMap = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(map[i][j] == BLAST){
+                    continue;
+                }
+                spreadDust(i,j,changedMap);
+            }
+        }
 
-        // V
-        for(int i=upBlastY; i>0; i--) {
+        return changedMap;
+    }
+
+    public static void spreadDust(int x, int y, int[][] changedMap){
+        ArrayList<Location> changedLocations = new ArrayList<>();
+
+        int origin = map[x][y];
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(isOutOfMap(nx,ny)){
+                continue;
+            }
+
+            if(map[nx][ny] == BLAST){
+                continue;
+            }
+
+            changedLocations.add(new Location(nx,ny));
+        }
+
+        int splited = origin/5;
+        changedMap[x][y] += origin - splited*changedLocations.size();
+        for(Location changedLocation:changedLocations){
+            changedMap[changedLocation.x][changedLocation.y] += splited;
+        }
+    }
+
+    public static boolean isOutOfMap(int nx, int ny){
+        return nx<0 || nx>=n || ny<0 || ny>=m;
+    }
+
+    public static void rotateUp(){
+        int x = blast[0];
+
+        for(int i=x;i>0;i--){ // 아래
             map[i][0] = map[i-1][0];
         }
 
-        // <
-        for(int i=0; i<M-1; i++) {
-            map[0][i] = map[0][i+1];
+        for(int i=1;i<m;i++){ // 왼쪽방향
+            map[0][i-1] = map[0][i];
         }
 
-        // ^
-        for(int i=0; i<upBlastY; i++) {
-            map[i][M-1] = map[i+1][M-1];
+        for(int i=1;i<=x;i++){
+            map[i-1][m-1] = map[i][m-1];
         }
 
-        // >
-        for(int i=M-1; i>0; i--) {
-            map[upBlastY][i] = map[upBlastY][i-1];
+        for(int i=m-1;i>0;i--){ // 오른쪽
+            map[x][i] = map[x][i-1];
         }
 
-        map[upBlastY][1] = 0;
-        map[upBlastY][0] = -1;
+        map[x][1] = 0;
+        map[x][0] = -1;
     }
 
-    // 아래쪽 바람
-    public static void downWind() {
-        Point downBlast = blast.get(1);
-        int downBlastY = downBlast.y;
+    public static void rotateDown(){
+        int x = blast[1];
 
-        // ^
-        for(int i=downBlastY; i<N-1; i++) {
+        for(int i=x;i<n-1;i++){
             map[i][0] = map[i+1][0];
         }
 
-        // <
-        for(int i=0; i<M-1; i++) {
-            map[N-1][i] = map[N-1][i+1];
+        for(int i=1;i<m;i++){ // 왼쪽방향
+            map[n-1][i-1] = map[n-1][i];
         }
 
-        // V
-        for(int i=N-1; i>downBlastY; i--) {
-            map[i][M - 1] = map[i - 1][M - 1];
+        for(int i=n-1;i>x;i--){
+            map[i][m-1] = map[i-1][m-1];
         }
 
-        // >
-        for(int i=M-1; i>0; i--) {
-            map[downBlastY][i] = map[downBlastY][i-1];
+        for(int i=m-1;i>0;i--){
+            map[x][i] = map[x][i-1];
         }
 
-        map[downBlastY][1] = 0;
-        map[downBlastY][0] = -1;
-    }
-
-    // 먼지 확산
-    public static void dustSpread(int y, int x) {
-        int spreadAmount = map[y][x] / 5;
-
-        for(int i=0; i<4; i++) {
-            int nextY = y + dy[i];
-            int nextX = x + dx[i];
-
-            if(!check(nextY, nextX)) continue;
-            if(map[nextY][nextX] == -1) continue;
-
-            copyMap[nextY][nextX] += spreadAmount;
-            copyMap[y][x] -= spreadAmount;
-        }
-    }
-
-    // 시공의 돌풍의 청소
-
-
-
-    public static boolean check(int y, int x) {
-        if(y < 0 || x < 0 || x >= M || y >= N) {
-            return false;
-        }
-        return true;
-    }
-
-    public static int countDustAnswer() {
-        int count = 0;
-        for(int i=0; i<N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == -1) continue;
-
-                count += map[i][j];
-            }
-        }
-        return count;
+        map[x][1] = 0;
+        map[x][0] = -1;
     }
 }
